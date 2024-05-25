9dd02eaf26449c40b878b78d27122c0ba4db79b9
Shojol Ahmed <shojolahmed271@gmail.com>
2024-05-26 05:27:06 +0600

    Add user screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -13,56 +13,58 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 32,
-                horizontal: 16,
-              ),
-              child: Text(
-                'Looking for Something?',
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontSize: 36,
-                  fontWeight: FontWeight.bold,
-                  color: kPrimaryTextColor,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 32,
+                  horizontal: 16,
                 ),
-              ),
-            ),
-            TextField(
-              onTapOutside: (event) {
-                FocusManager.instance.primaryFocus?.unfocus();
-              },
-              decoration: InputDecoration(
-                hintText: "Search",
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: const EdgeInsets.symmetric(
-                  horizontal: kHorizontalPadding,
-                  vertical: 16,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius:
-                      const BorderRadius.all(Radius.circular(kBorderRadius)),
-                  borderSide: BorderSide(
-                    color: kSecondaryTextColor.withOpacity(0.5),
-                    width: 0,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius:
-                      const BorderRadius.all(Radius.circular(kBorderRadius)),
-                  borderSide: BorderSide(
-                    color: kPrimaryColor.withOpacity(0.5),
-                    width: 1,
+                child: Text(
+                  'Looking for Something?',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: kPrimaryTextColor,
                   ),
                 ),
               ),
-            )
-          ],
+              TextField(
+                onTapOutside: (event) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: kHorizontalPadding,
+                    vertical: 16,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(kBorderRadius)),
+                    borderSide: BorderSide(
+                      color: kSecondaryTextColor.withOpacity(0.5),
+                      width: 0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(kBorderRadius)),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor.withOpacity(0.5),
+                      width: 1,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,10 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:fooder_app/values/colors.dart';
+import 'package:fooder_app/values/constants.dart';
 
 class UserScreen extends StatelessWidget {
   const UserScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('User'));
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: kHorizontalPadding,
+            vertical: 24,
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage("https://i.imgur.com/BoN9kdC.png"),
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Name, U.',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: kPrimaryTextColor,
+                            ),
+                          ),
+                          Text(
+                            'username@demomail.com',
+                            style: TextStyle(color: kSecondaryTextColor),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              UserScreenButton(
+                label: 'General',
+                onPressed: () {},
+              ),
+              const Divider(),
+              UserScreenButton(
+                label: 'User Settings',
+                onPressed: () {},
+              ),
+              const Divider(),
+              UserScreenButton(
+                label: 'About Us',
+                onPressed: () {},
+              ),
+              const Divider(),
+              UserScreenButton(
+                label: 'Rate Us',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UserScreenButton extends StatelessWidget {
+  const UserScreenButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+  });
+
+  final String label;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        minimumSize: const Size(double.infinity, 60),
+        shape: const BeveledRectangleBorder(),
+        alignment: Alignment.centerLeft,
+        foregroundColor: kPrimaryColor,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        label,
+        style: const TextStyle(
+          fontSize: 18,
+        ),
+      ),
+    );
   }
 }
