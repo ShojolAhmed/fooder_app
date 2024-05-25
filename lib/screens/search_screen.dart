fe32d5ff3a3f3ddb7121d9db21cd6404e45435d8
Shojol Ahmed <shojolahmed271@gmail.com>
2024-05-26 04:48:50 +0600

    Add search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fooder_app/values/colors.dart';
+import 'package:fooder_app/values/constants.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -10,6 +12,59 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Search'));
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 32,
+                horizontal: 16,
+              ),
+              child: Text(
+                'Looking for Something?',
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: kPrimaryTextColor,
+                ),
+              ),
+            ),
+            TextField(
+              onTapOutside: (event) {
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              decoration: InputDecoration(
+                hintText: "Search",
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.symmetric(
+                  horizontal: kHorizontalPadding,
+                  vertical: 16,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius:
+                      const BorderRadius.all(Radius.circular(kBorderRadius)),
+                  borderSide: BorderSide(
+                    color: kSecondaryTextColor.withOpacity(0.5),
+                    width: 0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius:
+                      const BorderRadius.all(Radius.circular(kBorderRadius)),
+                  borderSide: BorderSide(
+                    color: kPrimaryColor.withOpacity(0.5),
+                    width: 1,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
